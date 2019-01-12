63d6add8d9bd10359f0aa734f050bd9c2626ddb9
Саша <sasha.qudrov@gmail.com>
2019-01-13 00:40:56 +0300

    sana

--- JavaMessenger/Server/src/ru/chat/server/chatServer.java
+++ JavaMessenger/Server/src/ru/chat/server/chatServer.java
@@ -4,6 +4,7 @@ import ru.network.TCPConnection;
 import ru.network.TCPConnectionListener;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.util.ArrayList;
 
@@ -17,7 +18,8 @@ public class chatServer implements TCPConnectionListener {
     private static int Client_count = 0;//количество клиентов в чате
 
     private  final ArrayList<TCPConnection> connections = new ArrayList<>();//список из TCP соединений
-
+    private final ArrayList<String> messages=new ArrayList<>();//сообщения
+    private final ArrayList<TCPConnection> members=new ArrayList<>();//участники
     private  chatServer(){
         System.out.println("Server running...");
         try(ServerSocket serverSocket = new ServerSocket(8189)) {
@@ -36,10 +38,16 @@ public class chatServer implements TCPConnectionListener {
     @Override
     public synchronized void onConnectionReady(TCPConnection tcpConnection) {
         connections.add(tcpConnection);
+        if(messages.size()!=0) {
+            for(int i=0; i<messages.size();i++) {
+               tcpConnection.sendString(messages.get(i));
+                System.out.println(messages.get(i));
+            }
+            System.out.println("all");
 
-//        Client_count++;//считаем количество подключенных
+        }
+        //        Client_count++;//считаем количество подключенных
 //        sendToAllConnections("Количесво участников " + Client_count ); //выводим количество подключенных
-
         sendToAllConnections("Client connected " + tcpConnection );
     }
 
@@ -64,6 +72,10 @@ public class chatServer implements TCPConnectionListener {
     }
 
     private void sendToAllConnections(String value){
+        messages.add(value);
+        if(messages.size()>100){
+            messages.remove(1);
+        }
         System.out.println(value);
         final int cnt = connections.size();
         for (int i = 0; i < cnt; i++){
